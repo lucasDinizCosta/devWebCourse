936a6c0df0b1de762fe3d9c2c3c883198a7061c8
lucasDinizCosta <lucasdinizcostaprofessional@gmail.com>
2021-05-04 22:52:02 -0300

    IIFE

---
+++ Exercicios-JS/funcao/iife.js
@@ -0,0 +1,6 @@
+// IIFE -> Immediately Invoked Function Expression
+// Função imediatamente invocada
+(function(){
+    console.log('Será executada na hora!');
+    console.log('Foge do escopo mais abrangente!');
+})()
