48974dec04d184971f383f8bd9fde6738f6dbdd8
lucasDinizCosta <lucasdinizcostaprofessional@gmail.com>
2021-05-04 22:18:20 -0300

    Função Factory #01

---
+++ Exercicios-JS/funcao/factory1.js
@@ -0,0 +1,11 @@
+// Função factory: Função que retorna e cria um objeto
+
+// Factory simples
+function criarPessoa(){
+    return {
+        nome: 'Ana',
+        sobrenome: 'Silva'
+    }
+}
+
+console.log(criarPessoa());
