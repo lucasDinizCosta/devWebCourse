ccc2c3bc660c801850a3cd8a2b5d496c7cfc7db4
lucasDinizCosta <lucasdinizcostaprofessional@gmail.com>
2021-05-04 22:46:17 -0300

    Desafio da Função Construtora

--- Exercicios-JS/funcao/classeVsFactory.js
+++ Exercicios-JS/funcao/classeVsFactory.js
@@ -1,3 +1,9 @@
+/* Executando no navegador:
+
+Executando o document.getElementByTagName('body').onclick = p1.falar
+Gera 'Meu nome é undefined'
+Logo pode dar problema com o 'this' dentro da class Pessoa
+*/
 class Pessoa{
     constructor(nome){
         this.nome = nome
@@ -16,6 +22,13 @@ const criarPessoa = nome => {
         falar: () => console.log(`Meu nome é ${nome}`)
     }
 }
+
+/* Executando no navegador:
+
+Executando o document.getElementByTagName('body').onclick = p2.falar
+Gera 'Meu nome é João'
+O objeto usando a função factory tem ideia do seu contexto lexico de declaração
+*/
 
 const p2 = criarPessoa('João')
 p2.falar()
