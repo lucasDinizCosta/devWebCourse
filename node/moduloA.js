93c6161f7def3f1e1d6b7cfc9c7abb439dc97f3f
lucasDinizCosta <lucasdinizcostaprofessional@gmail.com>
2021-05-08 18:50:49 -0300

    Node - Sistema de modulos

---
+++ node/moduloA.js
@@ -0,0 +1,4 @@
+// Métodos para exportar elementos
+this.ola = 'Fala Pessoal'
+exports.bemVindo = 'Bem vindo ao node!'
+module.exports.ateLogo = 'Até próximo exemplo'
---
+++ node/moduloB.js
@@ -0,0 +1,7 @@
+// Forma mais clássica de exportação no node
+module.exports = {
+    bomDia: 'Bom dia',
+    boaNoite(){
+        return 'Boa noite'
+    }
+}
